bc3618932d524ac746e45ada7354d7eaed2c723d
HayateIwamoto <iwamoto.hayate.613@gmail.com>
2025-10-24 00:16:19 +0900

    feat: CountButtonの追加

---
+++ src/components/CountButton.tsx
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react";
+
+export const CountButton = () => {
+  const [count, setCount] = useState<number>(0);
+  const handleClick = () => {
+    setCount((prev: number) => prev + 1);
+  };
+  useEffect(() => {
+      console.log('Counter mounted:', new Date().toLocaleTimeString());
+    }, []);
+  return (
+    <>
+      <button onClick={handleClick} style={{marginBottom: "600px"}}>ボタンを押してください！</button>
+      <p>現在押された回数は{count}回です！</p>
+    </>
+  );
+};
