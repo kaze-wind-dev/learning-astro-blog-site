c600c6908b13c24e87b8a543bef72cbf9b8bff8d
HayateIwamoto <iwamoto.hayate.613@gmail.com>
2025-10-26 12:49:35 +0900

    SSRの動作確認

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import netlify from '@astrojs/netlify';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  // output: 'server',
+  output: 'static',
   adapter: netlify(),
 });
